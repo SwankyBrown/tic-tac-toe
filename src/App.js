9f7460006ba2a10ebacb33c27b84a52ccabe0468
Sheldon Christiansen <bigbrownguy724@gmail.com>
2023-06-08 13:04:18 -0600

    tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Square from './components/Square';
+import Video from './components/Video';
+
+const calculateWinner = (arr) => {
+  const lines = [
+    //**Horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    //**Diagonal
+    [0, 4, 8],
+    [6, 4, 2],
+    //**Vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+      return `${arr[a]} Wins!`;
+    }
+  }
+
+  return "Who will Come Out Victorious!";
+};
 
 function App() {
+  const [squares, setSquares] = useState(['', '', '', '', '', '', '', '', '']);
+  const [player, setPlayer] = useState(true);
+
+  const handleClick = () => {
+    setSquares(['', '', '', '', '', '', '', '', '']);
+    setPlayer(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Video />
+      <span>{calculateWinner(squares)}</span>
+      <div className="container">
+        {squares.map((val, index) => (
+          <Square
+            key={index}
+            squares={squares}
+            index={index}
+            setSquares={setSquares}
+            squareValue={val}
+            player={player}
+            setPlayer={setPlayer}
+          />
+        ))}
+        <button onClick={handleClick}>Reset</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Video.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Video = () => {
+  return (
+    <div>
+        <h1>HYPE TIC-TAC-TOE MUSIC</h1>
+        <iframe width="355" height="200" src="https://www.youtube.com/embed/VDvr08sCPOc" title="Remember The Name (Official Video) - Fort Minor" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+  );
+};
+
+export default Video;
